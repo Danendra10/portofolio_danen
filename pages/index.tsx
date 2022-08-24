674f509524bc3fb43d8d9969e8cdb78818999bf7
Danendra <5022201123@mhs.its.ac.id>
2022-08-24 09:59:58 +0700

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,44 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import {motion} from 'framer-motion'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 dark:bg-slate-800 dark:text-white">
       <Head>
-        <title>Create Next App</title>
+        <title>Danendra&apos;s Portof</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <motion.div initial="hidden" animate="visible" variants={{
+            hidden: {
+              opacity: 0,
+              y: -100,
+            },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: {
+                delay: 0.5,
+                duration: 0.5,
+              },
+            },
+          }}>
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+          <a className="text-blue-700">
+            My Portofolio
           </a>
         </h1>
+        </motion.div>
+        
+        <motion.div className='mt-20' whileHover={{scale:1.2}} whileTap={{scale: 0.8}}>
+          <button className="rounded-full bg-blue-50 text-white font-bold py-2 px-4">See More</button>
+        </motion.div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+        {/* <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
@@ -65,10 +78,10 @@ const Home: NextPage = () => {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div>
+        </div> */}
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
+      {/* <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center gap-2"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -78,7 +91,7 @@ const Home: NextPage = () => {
           Powered by{' '}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
